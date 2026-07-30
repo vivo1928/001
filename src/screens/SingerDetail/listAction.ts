dfafe0239a989ae354c46b3e6da182e29bbe7270
vivo1928 <vivo1928@users.noreply.github.com>
2026-07-30 10:35:01 +0000

    fix: 歌手详情页点击歌曲无法播放 - 移除重复 toNewMusicInfo 调用
    根因：MusicList.tsx 的 fetchList 已对返回歌曲调用了 toNewMusicInfo 转换格式，
    但 listAction.ts 的 handlePlay 又对列表再次调用 toNewMusicInfo，
    导致已转换的 meta._qualitys 被覆盖为 undefined，播放器无法解析音源
    
    修复：移除 listAction.ts 中冗余的 toNewMusicInfo 调用

--- src/screens/SingerDetail/listAction.ts
+++ src/screens/SingerDetail/listAction.ts
@@ -1,13 +1,13 @@
 import { setTempList } from '@/core/list'
 import { playList } from '@/core/player/player'
 import { LIST_IDS } from '@/config/constant'
-import { toNewMusicInfo } from '@/utils'
 
 const getListId = (id: string, source: LX.OnlineSource) => `${source}__${id}`
 
 export const handlePlay = async(id: string, source: LX.OnlineSource, list: LX.Music.MusicInfoOnline[], index = 0) => {
   const listId = getListId(id, source)
-  const targetList = list.map(s => toNewMusicInfo(s) as LX.Music.MusicInfoOnline)
-  await setTempList(listId, [...targetList])
+  // 注意：list 在 MusicList.tsx 的 fetchList 中已经通过 toNewMusicInfo 转换过格式
+  // 不要再次调用 toNewMusicInfo，否则会将已转换的 meta._qualitys 覆盖为 undefined
+  await setTempList(listId, [...list])
   void playList(LIST_IDS.TEMP, index)
 }
